267d98db20685f101527c3dd30f494551326c073
ilutskyi <lutskiigor@gmail.com>
2026-02-24 11:25:09 +0200

    modify paths

--- player.js
+++ player.js
@@ -1,9 +1,9 @@
-import renderPlayList from "./render.js";
+import renderPlayList from "./js/render.js";
 
 const playlists = [
     {
         title: "Hip Hop Hits",
-        coverImageUrl: "../src/img/istockphoto-1269075876-612x612.jpg",
+        coverImageUrl: "./src/img/istockphoto-1269075876-612x612.jpg",
         info: {
             totalTtrackCount: 4,
             totalTracksDurationInSeconds: 1008,
@@ -13,27 +13,27 @@ const playlists = [
                 title: "Straight Outta Compton",
                 artistName: "N.W.A",
                 fileUrl:
-                    "../src/mp3/N.W.A_-_Quiet_On_Tha_Set_Golos_ulic_2015_Straight_Outta_Compton_(mp3.pm).mp3",
-                imageUrl: "../src/img/Straight_Outta_Compton_logo.jpg",
+                    "./src/mp3/N.W.A_-_Quiet_On_Tha_Set_Golos_ulic_2015_Straight_Outta_Compton_(mp3.pm).mp3",
+                imageUrl: "./src/img/Straight_Outta_Compton_logo.jpg",
             },
             {
                 title: "One Mic",
                 artistName: "Nas",
-                fileUrl: "../src/mp3/Nas - One Mic (Official HD Video).mp3",
-                imageUrl: "../src/img/artworks-001016415490-n8t0lg-t500x500.jpg",
+                fileUrl: "./src/mp3/Nas - One Mic (Official HD Video).mp3",
+                imageUrl: "./src/img/artworks-001016415490-n8t0lg-t500x500.jpg",
             },
             {
                 title: "Forgot About Dre",
                 artistName: "Dr. Dre ft. Eminem",
                 fileUrl:
-                    "../src/mp3/ForgotAboutDre.mp3",
-                imageUrl: "../src/img/artworks-h38vVbu1Dy1GuBo1-Nngjpw-t1080x1080.jpg",
+                    "./src/mp3/ForgotAboutDre.mp3",
+                imageUrl: "./src/img/artworks-h38vVbu1Dy1GuBo1-Nngjpw-t1080x1080.jpg",
             },
             {
                 title: "It Was a Good Day",
                 artistName: "Ice Cube",
-                fileUrl: "../src/mp3/Ice Cube - It Was A Good Day.mp3",
-                imageUrl: "../src/img/images.jpg",
+                fileUrl: "./src/mp3/Ice Cube - It Was A Good Day.mp3",
+                imageUrl: "./src/img/images.jpg",
             },
         ],
     },
